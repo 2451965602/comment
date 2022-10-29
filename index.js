f79a2c60706ce5b5c6ab744e870781e36b9edd73
2451965602 <83867465+2451965602@users.noreply.github.com>
2022-10-29 19:41:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const Application = require('@waline/vercel');
-
+Waline.init({
+  locale: {
+    level0: '炼体',
+    level1: '炼气',
+    level2: '筑基',
+    level3: '金丹',
+    level4: '元婴',
+    level5: '化神',
+  },
+});
 module.exports = Application({
   async postSave(comment) {
     // do what ever you want after save comment
